2e9404f15ac6786cf3998416bba2c25a57dcf801
Matthew R. Rupp <ruppmatt@gmail.com>
2015-12-20 21:49:33 -0500

    Added additional comments to prepend.js

--- prepend.js
+++ prepend.js
@@ -1,23 +1,29 @@
 /*
- The purpose of this javascript prepended code is to:
- (1) keep consistant state information about avida in order to...
- (2) handle "file system" access rights and
- (3) make sure that worker-spawners have appropriate state information, also
- (4) provide message passing features such as
- (5) queuing messages when Avida is actively running
- (6) passing messages directly when avida is in an inactive state, and finally
- (7) provide functionality for Avida to pass messages to worker-spawners
-
- All function calls into and out of Avida handling messages will
- be points to strings of JSON objects.
+    This code gets prepended to the start of the emscripten
+    generated file 'test_cc.js' by the --prefix-js flag
+    during linking.  See the makefile.
  */
 
-var msg_queue = [];
 
+
+// MESSAGE UTILITIES -----------------------------------------------------------------------------
+//    The variables and utilities in this block are used for worker thread messaging.
+
+
+var msg_queue = [];  // Our message queue
+
+// The function that gets called when this thread receives a message
+// from its parent.
 onmessage = function (msg) {
     msg_queue.push(msg.data);
 }
 
+// The function that gets called from C++ to return
+// our message queue for processing.  Note that we
+// are writing a string to memory; we're relying on
+// the C++ side to clean up after ourselves.
+// (The JSON array message queue is being written to
+// the string).
 function doGetMessages() {
     var msgs = JSON.stringify(msg_queue);
     var buffer = _malloc(msgs.length + 1);
@@ -26,34 +32,63 @@ function doGetMessages() {
     return buffer;
 }
 
-
+// This function is called from the C++ side when we
+// want to send a message (in the form of a string
+// that is deserialized into a json object) to the
+// worker thread.
 function doPostMessage(msg_str) {
-    console.log(msg_str);
     var json_msg = JSON.parse(msg_str);
     self.postMessage(json_msg);
 }
 
+
+// FILE HANDLING -----------------------------------------------------------------------------
+//      The variables and functions in this block are used to handle the asynchronous
+//      nature of file synchronizing in emscripten's IDBFS.
+
+
+// FS_STATUS contains the state of the filesystem
+//    0  The file system is ready for reading and writing
+//    1  The file system is synchronizing
+//   -1  The file system encountered an error during syncing and should not be used
 var FS_STATUS = 0;
+
+
+// This function is called from the C++ side to perform the file syncing.
+// The populate variable controls whether data from the IndexedDB database
+// populates our file system.  (True =
+//
+// The FS.syncfs function is internal to emscripten.  The second parameter
+// is a callback function.   Because this operation is asynchronous, we need
+// to let the C++ side of the code know when the filesystem is ready.
+//
+// An FS_STAT < 1 indicates that we are done with trying to sync; FS_STAT > 1 indicates
+// we're still busy.
 function doSyncFS(populate) {
-    FS_STATUS = 1;
+    FS_STATUS = 1;  //Mark that we are syncing
     FS.syncfs(populate, function (err) {
+        //This block gets triggered when synchronizing is complete
         if (err) {
-            FS_STATUS = -1;
+            FS_STATUS = -1;  // On error, FS is not available
         }
         else {
-            FS_STATUS = 0;
+            FS_STATUS = 0;   // Syncing successful, the FS is available
         }
     })
 }
 
+
+// Utility function to check if there was a syncing error
 function fsError(){
     return FS_STATUS < 0;
 }
 
+// Utility function to check if the FS is busy syncing
 function fsBusy(){
     return FS_STATUS > 0;
 }
 
+// Utility function to check if the filesystem is ready for reading and writing
 function fsReady(){
     return FS_STATUS === 0;
 }
