0f5ea9ad09dc7d9ab8fd699ff18e7c51a5e92f96
Matthew R. Rupp <ruppmatt@gmail.com>
2016-01-18 20:02:08 -0500

    Updating to enable reading back from test.cc.  Still need to add utf confersions and file prototype.

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ function tryAgain(){
 
 
 /*
-    A function to add our status to the DOM.
+ A function to add our status to the DOM.
  */
 function display(text)
 {   var para = document.createElement("p");
@@ -33,8 +33,8 @@ function display(text)
 
 
 /*
-    The function that gets triggered when we get a message
-    from our worker thread.
+ The function that gets triggered when we get a message
+ from our worker thread.
  */
 function processMessage(incoming_msg){
     var msg = incoming_msg.data;
@@ -46,11 +46,13 @@ function processMessage(incoming_msg){
         } else if (msg.type === 'status') {
             if (msg.msg === 'wkr_fs_ready') {
                 display('Worker states: ' + msg.msg);
-                //setupStore();
-                tryEMSFileRead(root_path + '/ipsum.txt');
-            }
-            else {
-                display('Unknown status message: ' + msg.msg);
+                fs.read(root_path + '/ipsum.txt')
+                    .then(function (r) {
+                            display('Read /test/ipsum.txt as ' + r);
+                        }
+                        , function (err) {
+                            display('Could not read /test/ipsum.txt');
+                        });
             }
         }
         else {
@@ -61,36 +63,36 @@ function processMessage(incoming_msg){
 
 
 /*
-    This will setup our test environment.  I'm encapsulating
-    it in a function to see whether or not I can get the
-    web worker fully loaded (and waiting) before the tests
-    are performed.
+ This will setup our test environment.  I'm encapsulating
+ it in a function to see whether or not I can get the
+ web worker fully loaded (and waiting) before the tests
+ are performed.
  */
 function doTest() {
     fs.mount('/test').then(function() {
-        display('Successfully mounted directory.');
-        fs.write(root_path + '/disco.txt', 'Amazingly few discotheques provide jukeboxes.')
-            .then(function (result) {
-                    display('Wrote disco.txt')
-                },
-                function (err) {
-                    display('Cannot write disco.txt')
-                });
-        fs.read(root_path + '/disco.txt')
-            .then(function (result) {
-                    display('Read disco.txt' + result)
-                },
-                function (err) {
-                    display('Cannot read disco.txt')
-                });
-        fs.write(root_path + '/fox.txt', 'The quick brown fox jumped over the lazy dog.')
-            .then(function (result) {
-                    display('Wrote disco.txt')
-                },
-                function (err) {
-                    display('Cannot write fox.txt')
-                });
-    }, function(err) {display('Something unhandled happened.  Mounting perhaps?');}
+            display('Successfully mounted directory.');
+            fs.write(root_path + '/disco.txt', 'Amazingly few discotheques provide jukeboxes.')
+                .then(function (result) {
+                        display('Wrote disco.txt')
+                    },
+                    function (err) {
+                        display('Cannot write disco.txt')
+                    });
+            fs.read(root_path + '/disco.txt')
+                .then(function (result) {
+                        display('Read disco.txt' + result)
+                    },
+                    function (err) {
+                        display('Cannot read disco.txt')
+                    });
+            fs.write(root_path + '/fox.txt', 'The quick brown fox jumped over the lazy dog.')
+                .then(function (result) {
+                        display('Wrote disco.txt')
+                    },
+                    function (err) {
+                        display('Cannot write fox.txt')
+                    });
+        }, function(err) {display('Something unhandled happened.  Mounting perhaps?');}
     );
 }
 
